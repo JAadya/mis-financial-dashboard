5515585cf3340c8eac95ab25b498cf4a6c7bcb4f
JAadya <181739358+JAadya@users.noreply.github.com>
2026-07-14 23:20:41 +0530

    Connect frontend to deployed backend

--- client/src/api/dashboardApi.js
+++ client/src/api/dashboardApi.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-
-    baseURL: "http://localhost:5000/api"
-
+  baseURL: "https://mis-financial-dashboard.onrender.com/api",
 });
 
 export default API;
